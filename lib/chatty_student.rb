3fb1d822e01839e7c3774acefa45645167886659
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-08-22 10:53:05 +0300

    commit tree 201a21d5026bbb92a309b4c8b87be708af053fff

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,15 +7,6 @@ class ChattyStudent < Student
     end
 
     def raise_hand
-        super
-        super
-        super
-        super
-        super
-        super
-        super
-        super
-        super
-        super
+        10.times {|i| super }
     end
 end
